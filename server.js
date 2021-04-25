5fd68d21d851782bc1df98441ae732dbb589bd05
Pooja <47268884+pooja5101@users.noreply.github.com>
2021-04-25 10:56:12 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const experss = require('express')
+const app = express()
+/*
+app.set('view-engine' , 'ejs')
+
+app.get('/', (req, res) =>{
+  res.render('index.ejs',{ name :'Pooja'})
+})
+
+app.get('/login', (req, res) =>{
+  res.render('login.ejs')
+})
+
+app.get('/register', (req, res) =>{
+  res.render('register.ejs')
+})
+*/
+app.listen(3000)
